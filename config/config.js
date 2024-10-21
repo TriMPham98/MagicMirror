4a9bfb101a90d4980975442809b2ad9751da21d6
Tri Pham <trimpham98@gmail.com>
2024-10-20 21:06:37 -0700

    Replace APOD with JARVIS animation

--- config/config.js
+++ config/config.js
@@ -85,30 +85,13 @@ let config = {
             }
         },
         {
-            module: "MMM-APOD",
-            position: "fullscreen_below",
+            module: "MMM-IronManGIF",
+            position: "middle_center",
             config: {
-                appid: "UzbYQacVqdlHwYdAsgH6gsQWnPEjZqkOElHntK0h", // Replace with your actual NASA API key
-                updateInterval: 5 * 60 * 60 * 1000, // Update every 5 hours
-                animationSpeed: 1000,
-                maxMediaWidth: 0, // 0 means use actual width
-                maxMediaHeight: 0, // 0 means use actual height
-                maxDescriptionLength: 0, // 0 means no limit
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundOverlay: "linear-gradient(to bottom, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 0.75) 100%)",
-                showTitle: true,
-                showDescription: true,
-                useShortDescription: false,
-                initialLoadDelay: 0,
-                retryDelay: 2500,
-                apiBase: "https://api.nasa.gov/",
-                apodEndpoint: "planetary/apod",
-                titleLocation: "bottomCenter",
-                descriptionLocation: "bottomCenter",
-                numberOfDays: 30, // Fetch images from the last 30 days
-                cycleWithoutFetch: true, // Cycle through fetched images without re-fetching
-                cycleDuration: 5 * 60 * 1000 // Change image every 5 minutes
+                style: 4,              // Changed to style 4
+                maxWidth: "100%",      // Sizes the images. 
+                rotate: false,         // Rotate through images
+                updateInterval: 30000  // Interval between image rotations (30 seconds)
             }
         }
     ]
